7d6bb29ec4bbc315a8941b82859d0d426be1c38e
Sanskriti <sanskriti.agarwal.sa@gmail.com>
2021-08-22 20:10:13 +0530

    updated MordernJS.js and ReadME

--- MordernJS.js
+++ MordernJS.js
@@ -1,24 +1,129 @@
+// promises
+//1. are async
 
+const myPromisesFunc = (num) => {
+  return new Promise((resolve, reject) => {
+    if (num > 5) {
+      resolve('Greater than 5');
+    } else {
+      reject('Less than 5');
+    }
+  });
+};
 
+async function myPromisesEx() {
+  try {
+    const val = await myPromisesFunc(1);
+    console.log(val);
+  } catch (err) {
+    console.log('ERROR: ', err);
+  }
+}
 
+myPromisesEx();
 
+// myPromisesFunc(5)
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log('ERROR: ', err);
+//   });
 
+// myPromises
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log('Error:', err);
+//   });
 
+// Rest and spread operators
+const person = {
+  name1: 'abc',
+  age2: 19,
+  hobby: 'designing',
+};
 
+const extendedPerson = {
+  ...person,
+  school: 'ihj',
+};
 
+console.log(extendedPerson);
 
+const { name, ...anotherPerson } = person;
 
+console.log(anotherPerson);
 
+const anotherPersonRelation = {
+  ...extendedPerson,
+  relation: 'brother',
+  name1: 'xyz',
+  ...anotherPerson,
+};
 
+console.log(anotherPersonRelation);
 
+//array
 
+const arr2 = ['this', 'is', 'an', 'array'];
 
+const arr3 = [5, 6, 7];
 
+const arr4 = [...arr2, 'some', 'value', ...arr3];
 
+console.log(arr4);
 
+function letsReduce(first, ...rest) {
+  console.log(rest);
+}
 
+letsReduce(arr3);
 
+// object and array destructuring
 
+//1.
+
+const hobby = 'webdev';
+
+const { name5, age2, hobby: hobby5 } = person;
+
+console.log(name5, age2, hobby5);
+
+//2.
+
+const [first, , , fourth] = arr2;
+
+console.log(fourth);
+
+//3.
+
+function somefun({ name1 } = {}, { fourth } = []) {
+  console.log(name1, fourth);
+} // fourth will give undefined as the array is not passed but default argument undefined is set, without that it will give an error
+
+somefun(person);
+
+// string interpolation
+
+const age = 19;
+const name2 = 'kole';
+const getName = () => 'full name';
+
+const compString = `My name is ${name2}. My age is ${age}`;
+const compString2 = `My full name is ${getName()}`;
+
+console.log(compString);
+console.log(compString2);
+
+// Arrow function and default argument
+
+const arrowF = () => {
+  return 10;
+};
+
+console.log(arrowF());
 
 // Var vs Let vs Const
 function wow() {
@@ -63,7 +168,7 @@ function functionDeclaration() {}
 
 //functionDeclaration(); //will work
 
-functionExpression(); // won't work
+//functionExpression(); // won't work
 
 const functionExpression = function () {};
 
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This repository contains basic knowledge about react JS and is currently incomplete.
 
 Includes:
+
 - how to import dependencies (cowsay) and other javascript files (exports and require)
 - Modern Javascript
 - downloaded prettier and eslint as dependencies.
@@ -8,12 +9,26 @@ Includes:
 
 Cowsay code implementation:
 
- -------
+---
+
 < Hello >
- -------
+
+---
+
         \   ^__^
 
          \  (oO)\_______
             (__)\       )\/\
              U  ||----w |
                 ||     ||
+
+MordernJS.js consist of examples under:
+-> Var vs const vs let
+-> array.map
+-> array.reduce
+-> function expression vs function decleration
+-> Arrow Function and default argument
+-> string interpolation
+-> object and array destructring
+-> spread and rest objects
+-> Async and Sync Code - Promises and Async Await
